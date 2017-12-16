c06d295251afce5aed6de82089dd6cd6b166a796
Luca Pasquale <lucapasquale.094@gmail.com>
2017-12-16 00:47:36 -0200

    economy command

--- src/commands/economy/index.ts
+++ src/commands/economy/index.ts
@@ -1,10 +1,28 @@
 import * as Discord from 'discord.js';
-import * as redditApi from './reddit-api';
+import axios from 'axios';
 
+
+const LIMIT = 10;
+
+const redditApi = axios.create({
+  baseURL: 'https://www.reddit.com',
+});
 
 export default async function (cmds: string[], message: Discord.Message) {
-  const comments = await redditApi.getBestComments('memeEconomy', '7jzdnj');
-  console.log(comments);
+  const { data } = await redditApi.get('r/MemeEconomy/top.json', {
+    params: {
+      t: 'day',
+      limit: LIMIT,
+    },
+  });
+
+  const topPosts = data.data.children;
+  const randomPost = topPosts[Math.floor(Math.random() * LIMIT)];
+
+  console.log(randomPost);
+  await message.channel.send('Investment opportunity of the day', {
+    embed: generateEmbed(randomPost),
+  });
 }
 
 
