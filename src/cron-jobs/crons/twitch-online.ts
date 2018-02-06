70325934dbc94b22017116b5fbc1235acd65172c
Luca Pasquale <lucapasquale.094@gmail.com>
2018-02-05 22:27:49 -0200

    fixed crons initialization, fixed lol-team command (#14)

--- src/cron-jobs/crons/twitch-online.ts
+++ src/cron-jobs/crons/twitch-online.ts
@@ -6,33 +6,29 @@ import { Db } from '../../types';
 import config from '../../config';
 
 
-export default async function (channel: Discord.TextChannel, db: Db) {
-  try {
-    const twitchStreams = await db.Streams.findAll({
-      where: { service: 'twitch' },
-    });
-
-    await bluebird.each(twitchStreams, async (stream) => {
-      const { online, data } = await getStreamData(stream.token);
-
-      if (stream.online !== online) {
-        if (online) {
-          const { content, embed } = await createMessage(stream.token, data);
-          await channel.send(content, { embed });
-        }
-
-        await stream.update({ online });
-      }
-    });
-  }
-  catch (e) { }
-}
-
-
 const twitchRequest = axios.create({
   baseURL: 'https://api.twitch.tv/helix',
   headers: { 'Client-ID': config.TWITCH_KEY },
 });
+
+export default async function (channel: Discord.TextChannel, db: Db) {
+  const twitchStreams = await db.Streams.findAll({
+    where: { service: 'twitch' },
+  });
+
+  await bluebird.each(twitchStreams, async (stream) => {
+    const { online, data } = await getStreamData(stream.token);
+
+    if (stream.online !== online) {
+      if (online) {
+        const { content, embed } = await createMessage(stream.token, data);
+        await channel.send(content, { embed });
+      }
+
+      await stream.update({ online });
+    }
+  });
+}
 
 async function getStreamData(token: string) {
   const { data } = await twitchRequest.get(`streams?user_login=${token}`);
@@ -45,7 +41,6 @@ async function getUserData(userId: number) {
   const { data } = await twitchRequest.get(`users?id=${userId}`);
   return data.data[0];
 }
-
 
 async function createMessage(token: string, streamData: any) {
   const { user_id, title, viewer_count } = streamData;
--- src/cron-jobs/index.ts
+++ src/cron-jobs/index.ts
@@ -3,19 +3,24 @@ import * as later from 'later';
 
 import { Db, Cron } from '../types';
 import crons from './crons';
-import { getDefaultSettings } from 'http2';
 
 
 export function startCrons(client: Discord.Client, db: Db) {
   const textChannel = getDefaultTextChannel(client.channels.array());
 
   crons.map(({ handler, interval }) => {
-    later.setInterval(() => handler(textChannel, db), later.parse.text(interval));
+    later.setInterval(() => {
+      try {
+        handler(textChannel, db);
+      } catch (e) {
+        console.error(e);
+      }
+    }, later.parse.text(interval));
   });
 }
 
 
 function getDefaultTextChannel(channels: Discord.Channel[]) {
-  const firstTextChannel = channels.find((c: Discord.Channel) => c.type === 'text');
-  return firstTextChannel as Discord.TextChannel;
+  const firstTextChannel = channels.filter((c: Discord.Channel) => c.type === 'text');
+  return firstTextChannel[0] as Discord.TextChannel;
 }
