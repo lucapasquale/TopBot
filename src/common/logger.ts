7d1908cbce86b674439a21a4e4b80752d97cc4ad
Luca Pasquale <lucapasquale.094@gmail.com>
2018-06-04 21:17:55 -0300

    added timestamp to logs (#18)

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -5,13 +5,17 @@ export function createLogger() {
     level: 'debug',
 
     format: winston.format.combine(
+      winston.format.timestamp(),
       winston.format.colorize(),
       winston.format.json(),
     ),
 
     transports: [
       new winston.transports.Console({
-        format: winston.format.simple(),
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.simple(),
+        ),
       }),
 
       new winston.transports.File({ filename: 'error.log', level: 'error' }),
