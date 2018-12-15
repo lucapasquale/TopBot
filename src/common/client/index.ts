2a0b2547e32c495049d0178d0542c874a5ecb27c
Luca Pasquale <lucapasquale.094@gmail.com>
2018-12-15 20:13:09 -0200

    client on error (#24)

--- src/common/client/index.ts
+++ src/common/client/index.ts
@@ -1,8 +1,7 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import { Client } from 'discord.js';
 
-import { Logger, Database, Command } from '../../types';
+import { Logger, Database } from '../../types';
+import { getAllCommands } from './helpers';
 import config from '../../config';
 import onReady from './on-ready';
 import onMessage from './on-message';
@@ -21,34 +20,11 @@ export async function startClient(log: Logger, db: Database) {
   client.on('message', async message => {
     await onMessage(message, ctx);
   });
-}
 
-function getAllCommands(commandsPath: string): Command[] {
-  const cmds = [] as string[];
-
-  fs.readdirSync(commandsPath).forEach(folder => {
-    if (hasHandler(commandsPath, folder)) {
-      cmds.push(folder);
-      return;
-    }
-
-    const folderPath = path.join(commandsPath, folder);
-    fs.readdirSync(folderPath).forEach(subFolder => {
-      if (hasHandler(folderPath, subFolder)) {
-        cmds.push(path.join(folder, subFolder));
-      }
+  client.on('error', async error => {
+    log.error('Client error!', {
+      message: error.message,
+      stack: error.stack,
     });
-  });
-
-  return cmds.map(job => {
-    const definition = module.require(path.join(commandsPath, job)).default;
-    return definition;
-  });
-}
-
-function hasHandler(basePath: string, folderName: string): boolean {
-  return ['js', 'ts'].some(fileType => {
-    const filePath = path.join(basePath, folderName, `handler.${fileType}`);
-    return fs.existsSync(filePath);
   });
 }
