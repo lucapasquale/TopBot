8189060287a2e0408c4d398f799d0e2dac6c09c9
Luca Pasquale <lucapasquale.094@gmail.com>
2018-12-15 20:12:25 -0200

    client on error

---
+++ src/common/client/helpers.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Command } from '../../types';
+
+export function getAllCommands(commandsPath: string): Command[] {
+  const cmds = [] as string[];
+
+  fs.readdirSync(commandsPath).forEach(folder => {
+    if (hasHandler(commandsPath, folder)) {
+      cmds.push(folder);
+      return;
+    }
+
+    const folderPath = path.join(commandsPath, folder);
+    fs.readdirSync(folderPath).forEach(subFolder => {
+      if (hasHandler(folderPath, subFolder)) {
+        cmds.push(path.join(folder, subFolder));
+      }
+    });
+  });
+
+  return cmds.map(job => {
+    const definition = module.require(path.join(commandsPath, job)).default;
+    return definition;
+  });
+}
+
+function hasHandler(basePath: string, folderName: string): boolean {
+  return ['js', 'ts'].some(fileType => {
+    const filePath = path.join(basePath, folderName, `handler.${fileType}`);
+    return fs.existsSync(filePath);
+  });
+}
